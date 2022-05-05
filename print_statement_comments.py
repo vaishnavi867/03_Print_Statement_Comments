fbe9ed06f7c11c8a7f77411507a4ee58d052f2dd
vaishnavi867 <104981441+vaishnavi867@users.noreply.github.com>
2022-05-05 16:18:29 +0530

    Add files via upload

---
+++ print_statement_comments.py
@@ -0,0 +1,77 @@
+"""
+Name: HD
+Date & Day: 4th May 2022 , Wednesday
+Time:3:30-4:30PM
+File Description: print statement and types of comment
+"""
+# print('hello python')
+# print("hello world")
+
+# print('hello wor'ld') # SyntaxError
+
+'''To print  hello wor'ld    '''
+# print("hello wor'ld")
+
+'''To print  hello wor"ld    '''
+# print('hello wor"ld')
+
+# print('''python
+# programming 
+# course''')
+
+# print('''python
+#   programming 
+# course''')
+
+# print('''python
+
+# programming 
+# course''')
+
+
+# print('''python programming course''')
+
+# print('''pyth"on program'ming course''')
+
+
+# print("""python
+# programming 
+# course""")
+
+# print("""pyth"on cou'rse""")
+
+# print()
+
+# print('')
+# print(" ")
+# print('''
+# ''')
+# print("""
+
+# """)
+
+""" To print  hello world   """
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+
+
+""" To print  hello wor'ld   """
+# print('hello wor\'ld')
+# print("hello wor'ld")
+# print('''hello wor'ld''')
+# print("""hello wor'ld""")
+
+""" To print  hello wor"ld   """
+# print('hello wor"ld')
+# print("hello wor\"ld")
+# print('''hello wor"ld''')
+# print("""hello wor"ld""")
+
+
+""" To print    print(hello python)   """
+# print('print(hello python)')
+# print("print(hello python)")
+# print('''print(hello python)''')
+# print("""print(hello python)""")
